505a0735af137f8a244298a38608642dc2bb4263
ankitprahladsoni <ankitprahladsoni@gmail.com>
2020-06-05 02:06:59 -0700

    refactor

--- src/Problem/Problem.js
+++ src/Problem/Problem.js
@@ -7,13 +7,14 @@ import { Approach } from './Approach';
 import { Code } from './Code';
 
 export function Problem({ problem, setTag }) {
+  const { title, source, tags, approach, code } = problem;
   return (
     <Container maxWidth="lg">
       <br />
-      <Title title={problem['title']} source={problem['source']} />
-      <Tags updateRows={setTag} tags={problem['tags']} />
-      <Approach approach={problem['approach']} />
-      <Code docco={docco} code={problem['code']} />
+      <Title title={title} source={source} />
+      <Tags updateRows={setTag} tags={tags} />
+      <Approach approach={approach} />
+      <Code docco={docco} code={code} />
       <br />
     </Container>
   );
