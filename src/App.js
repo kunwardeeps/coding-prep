e4d5d6342ce8035d3245d9ff934503d1791ede68
ankitprahladsoni <ankitprahladsoni@gmail.com>
2020-06-05 01:38:43 -0700

    custom hooks

--- src/App.js
+++ src/App.js
@@ -1,47 +1,14 @@
 import { Body } from './Body';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Footer from './Footer';
 import GitHubForkRibbon from 'react-github-fork-ribbon';
 import { Loading } from './Loading';
-import fetchData from './api';
+import useData from './useData';
 
 export default function App() {
-  const [data, setData] = useState([]);
-  const [rows, setRows] = useState([]);
-  const [tag, setTag] = useState('all');
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    if (tag === 'all') {
-      setRows(data);
-    } else {
-      setRows(data.filter((entry) => entry['tags'].includes(tag)));
-    }
-  }, [tag, data]);
-
-  useEffect(() => {
-    (async function () {
-      const sheetData = await fetchData();
-      updateDataFromSheet(sheetData);
-      setIsLoading(false);
-    })();
-  }, []);
-
-  const updateDataFromSheet = (sheet) => {
-    let data = [];
-    for (let i = 2; sheet[`A${i}`]; i++) {
-      data.push({
-        title: sheet[`A${i}`].v,
-        source: sheet[`B${i}`]?.v,
-        tags: sheet[`C${i}`]?.v.replace(/\s+/g, '').split(','),
-        approach: sheet[`D${i}`]?.v,
-        code: sheet[`E${i}`]?.v,
-      });
-    }
-    setData(data);
-  };
+  const [tag, rows, isLoading, setTag] = useData();
 
   return isLoading ? (
     <Loading />
---
+++ src/useData.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+import fetchData from './api';
+
+const useData = () => {
+  const [data, setData] = useState([]);
+  const [rows, setRows] = useState([]);
+  const [tag, setTag] = useState('all');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (tag === 'all') {
+      setRows(data);
+    } else {
+      setRows(data.filter((entry) => entry['tags'].includes(tag)));
+    }
+  }, [tag, data]);
+
+  useEffect(() => {
+    (async function () {
+      const sheetData = await fetchData();
+      updateDataFromSheet(sheetData);
+      setIsLoading(false);
+    })();
+  }, []);
+
+  const updateDataFromSheet = (sheet) => {
+    let data = [];
+    for (let i = 2; sheet[`A${i}`]; i++) {
+      data.push({
+        title: sheet[`A${i}`].v,
+        source: sheet[`B${i}`]?.v,
+        tags: sheet[`C${i}`]?.v.replace(/\s+/g, '').split(','),
+        approach: sheet[`D${i}`]?.v,
+        code: sheet[`E${i}`]?.v.trim(),
+      });
+    }
+    setData(data);
+  };
+
+  return [tag, rows, isLoading, setTag];
+};
+
+export default useData;
